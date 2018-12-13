1ad71d759f575954e6f505ad2faec063adf8ce74
crowelmk <crowelmk@rose-hulman.edu>
2018-12-12 23:18:30 -0500

    Added depth to processing

--- KinectPointingAPI/Controllers/ObjectDetectionController.cs
+++ KinectPointingAPI/Controllers/ObjectDetectionController.cs
@@ -20,9 +20,9 @@ namespace KinectPointingAPI.Controllers
 {
     public struct BlockData
     {
-        public Point center;
+        public Vector3 center;
 
-        public BlockData(Point center)
+        public BlockData(Vector3 center)
         {
             this.center = center;
         }
@@ -95,11 +95,12 @@ namespace KinectPointingAPI.Controllers
         {
             Bitmap colorData = this.ConvertCurrFrameToBitmap();
             Point[] blockCenters = this.blockDetector.DetectBlocks(colorData, this.colorFrameDescription.Width, this.colorFrameDescription.Height);
+            List<Vector3> augmentedCenters = this.AugmentCentersWithDepth(blockCenters);
 
             List<BlockData> aggregatedData = new List<BlockData>();
-            for (int i = 0; i < blockCenters.Length; i++)
+            for (int i = 0; i < augmentedCenters.Count; i++)
             {
-                Point currCenter = blockCenters[i];
+                Vector3 currCenter = augmentedCenters[i];
                 BlockData currData = new BlockData(currCenter);
                 aggregatedData.Add(currData);
             }
@@ -130,6 +131,59 @@ namespace KinectPointingAPI.Controllers
                 bmp = new System.Drawing.Bitmap(outStream);
             }
             return bmp;
+        }
+
+        private List<Vector3> AugmentCentersWithDepth(Point[] centers)
+        {
+            int depthWidth = this.currDepthFrame.FrameDescription.Width;
+            int depthHeight = this.currDepthFrame.FrameDescription.Height;
+            ushort[] depths = new ushort[depthWidth * depthHeight];
+            this.currDepthFrame.CopyFrameDataToArray(depths);
+
+            int colorWidth = this.colorFrameDescription.Width;
+            int colorHeight = this.colorFrameDescription.Height;
+            DepthSpacePoint[] depthPoints = new DepthSpacePoint[colorWidth * colorHeight];
+            kinectSensor.CoordinateMapper.MapColorFrameToDepthSpace(depths, depthPoints);
+
+            List<Vector3> augmentedCenters = new List<Vector3>();
+            foreach (Point center in centers)
+            {
+
+                int viableIdx = -1;
+                Boolean foundViableIndex = false;
+
+                // Find a nearby point for which the depth is actually defined, as depth resolution is smaller than color resolution -> not all color points have a depth
+                for(int i = -10; i < 10; i++)
+                {
+                    for(int j = -10; j < 10;  j++)
+                    {
+                        int colorIdx = (center.Y + j) * colorWidth + (center.X + i);
+                        
+                        if(depthPoints[colorIdx].X != Double.NegativeInfinity && depthPoints[colorIdx].Y != Double.NegativeInfinity)
+                        {
+                            System.Diagnostics.Debug.Print("i: " + i);
+                            System.Diagnostics.Debug.Print("j: " + j);
+                            viableIdx = colorIdx;
+                            foundViableIndex = true;
+                            break;
+                        }
+                    }
+
+                    if(foundViableIndex)
+                    {
+                        break;
+                    }
+                }
+
+                int depthX = foundViableIndex ? Convert.ToInt32(depthPoints[viableIdx].X) : 0;
+                int depthY = foundViableIndex ? Convert.ToInt32(depthPoints[viableIdx].Y) : 0;
+
+                int depthIdx = depthY * depthWidth + depthX;
+                int currDepth = Convert.ToInt32(depths[depthIdx]);
+                augmentedCenters.Add(new Vector3(center.X, center.Y, currDepth));
+            }
+
+            return augmentedCenters;
         }
     }
 }
